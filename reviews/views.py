8924ad1bdac78c57f77f64c225b69b60d36c4f3d
Muratbek Sharibekov <42814171+muraat2@users.noreply.github.com>
2021-11-13 23:42:48 +0600

    Added templates

---
+++ reviews/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, HttpResponse
+from .utils import average_rating
+
+
+# def index(request):
+#     name = "world"
+#     return render(request, "base.html", {"name": name})
+#
+#
+# def book_search(request):
+#     book_name = request.GET.get("search")
+#     return render(request, "searchResult.html", {"book_name": book_name})
+from reviews.models import Book
+
+
+def welcome_view(request):
+    return render(request, "base.html")
+
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({"book": book, "book_rating": book_rating, "number_of_reviews": number_of_reviews})
+
+    context = {
+        "book_list": book_list
+    }
+    return render(request, "reviews/books_list.html", context)
+
